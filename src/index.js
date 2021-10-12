2d4b7627e4d7f231f2226623df6088dcd02fa882
cjbaldoza <cjbaldoza@gmail.com>
2021-10-12 14:50:42 -0500

    Added additional example.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
-//Create a react app from scratch.
-//It should display 2 paragraph HTML elements.
-//The paragraphs should say:
-//Created by YOURNAME.
-//Copyright CURRENTYEAR.
-//E.g.
-//Created by Angela Yu.
-//Copyright 2019.
-
 import React from "react";
 import ReactDOM from "react-dom";
 
@@ -15,18 +6,27 @@ const dynamicImg = "https://picsum.photos/200";
 const fName = "Christopher";
 const lName = "Baldoza";
 
+const customStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: "1px solid black"
+};
+
+customStyle.color = "blue";
+
 const copyRightYear = new Date().getFullYear();
 console.log(copyRightYear);
 
 ReactDOM.render(
   <div>
+    <h1 style={customStyle}>Hello World!</h1>
     <h1 className="heading" contentEditable="true" spellCheck="false">
       My Favourite Foods
     </h1>
     <ul>
-      <li>Mongo</li>
+      <li>Ginisang Mongo</li>
       <li>Halabos na Hipon</li>
-      <li>Pancit Canton</li>
+      <li>Pancit Bihon</li>
     </ul>
   </div>,
   document.getElementById("header")
@@ -36,14 +36,17 @@ ReactDOM.render(
   <div>
     <img
       className="food-img"
+      alt="Ginisang Mongo"
       src="http://1.bp.blogspot.com/-K6ixGtpDJO0/T3funOj3ScI/AAAAAAAAAho/Q8dP8Garx_c/s1600/MONGO%2BSOUP.jpg"
     />
     <img
       className="food-img"
+      alt="Halabos na Hipon"
       src="https://2.bp.blogspot.com/-aouSjQyvjD0/W7bNR3SN8CI/AAAAAAAAMjA/YT_WB3o4dpoSkVCaTPhhDcAf-UnDjmbLACLcBGAs/w1200-h630-p-k-no-nu/20170909_125442a.jpg"
     />
     <img
       className="food-img"
+      alt="Pancit Bihon"
       src="https://www.whats4eats.com/files/pastas-pancit-bihon-flickr-dbgg1979-3852511838-4x3.jpg"
     />
   </div>,
@@ -52,7 +55,7 @@ ReactDOM.render(
 
 ReactDOM.render(
   <div>
-    <img src={dynamicImg} />
+    <img src={dynamicImg + "?grayscale"} alt="Random" />
   </div>,
   document.getElementById("carousel")
 );
